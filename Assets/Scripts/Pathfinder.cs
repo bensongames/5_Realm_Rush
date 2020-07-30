cbab3bc54a6415c1f6eb4a629d3f02889ad69028
Darren Benson <darren.benson@deskpoint.com>
2020-07-30 23:52:47 +0100

    134: Colour neighbouring cubes

--- Assets/Scripts/Pathfinder.cs
+++ Assets/Scripts/Pathfinder.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 [DisallowMultipleComponent]
@@ -10,17 +12,24 @@ public class Pathfinder : MonoBehaviour
     [SerializeField] private Waypoint _endWaypoint;
 
     private Dictionary<Vector2Int, Waypoint> _grid = new Dictionary<Vector2Int, Waypoint>();
+    private Vector2Int[] _directions = { 
+        Vector2Int.up,
+        Vector2Int.right,
+        Vector2Int.down,
+        Vector2Int.left
+    };
 
     private void Start()
     {
         InitialiseWaypoints();
+        ExploreNeighbours();
     }
 
     private void InitialiseWaypoints()
     {
         if (_startWaypoint != null)
         {
-            _startWaypoint.SetTopColor(Color.blue);
+            _startWaypoint.SetTopColor(Color.green);
         }
         if (_endWaypoint != null)
         {
@@ -45,6 +54,25 @@ public class Pathfinder : MonoBehaviour
                 _grid.Add(gridPosition, waypoint);
             }
             
+        }
+    }
+
+    private void ExploreNeighbours()
+    {
+        foreach(var waypoint in _grid) { 
+            foreach (var direction in _directions)
+            {
+                var gridPosition = waypoint.Value.GetGridPosition();
+                var explorePosition = gridPosition + direction;
+                if (_grid.TryGetValue(explorePosition, out Waypoint exploreWaypoint))
+                {
+                    exploreWaypoint.SetTopColor(Color.blue);
+                }
+                else
+                {
+                    Debug.Log($"No cube at explore position {explorePosition}");
+                }
+            }
         }
     }
 }
