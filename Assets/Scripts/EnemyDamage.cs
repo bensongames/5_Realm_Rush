103254308e8642041249df2ba815586eba8dec18
Darren Benson <darren.benson@deskpoint.com>
2020-08-01 11:30:58 +0100

    Towers shoot and destroy enemy

---
+++ Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    [Tooltip("Particle Effects")] [SerializeField] GameObject _explosionPrefab;
+    [SerializeField] private int _hitPoints = 10;
+
+    private void Start()
+    {
+        AddNonTriggerBoxCollider();
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+    }
+
+    private void AddNonTriggerBoxCollider()
+    {
+        var boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+
+    void ProcessHit()
+    {
+        _hitPoints -= 1;
+        if (_hitPoints <= 0)
+        {
+            Explode();
+        }
+    }
+
+    private void Explode()
+    {
+        Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/SelfDestruct.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class SelfDestruct : MonoBehaviour
+{
+
+    [SerializeField] private float _timeToDestruct = 5f;
+
+    void Start()
+    {
+        Destroy(gameObject, _timeToDestruct);
+    }
+
+}
