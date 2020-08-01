876755760766ecfe47a41fdf8070541bdc2aa4f3
Darren Benson <darren.benson@deskpoint.com>
2020-08-01 12:01:01 +0100

    Shoot at targets when in range

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -4,11 +4,44 @@ public class Tower : MonoBehaviour
 {
 
     [SerializeField] private Transform _gun;
+    [SerializeField] private ParticleSystem _bullets;
     [SerializeField] private Transform _target;
+    [SerializeField] private float _range = 30f;
 
-    void Update()
+    private void Update()
     {
+        FollowTarget();
+        FireAtTargetWhenInRange();
+    }
 
+    private void FollowTarget()
+    {
+        if (_target == null) return;
         _gun.LookAt(_target);
+    }
+
+    private void FireAtTargetWhenInRange()
+    {
+        if (TargetInRange())
+        {
+            FireWeapon(true);
+        }
+        else
+        {
+            FireWeapon(false);
+        }
+    }
+
+    private void FireWeapon(bool fire)
+    {
+            var bulletEmission = _bullets.emission;
+            bulletEmission.enabled = fire;
+    }
+
+    private bool TargetInRange()
+    {
+        if (_target == null) return false;
+        var distanceToTarget = Vector3.Distance(_target.position, transform.position);
+        return distanceToTarget <= _range;
     }
 }
